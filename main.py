9d11257877be2d6a2f32c7ea53ac56df8361610f
Jeff Olson <jolson615@gmail.com>
2021-07-20 17:11:06 +0000

    initial commit

---
+++ main.py
@@ -0,0 +1,31 @@
+from flask import Flask, render_template, request, redirect
+from flask_pymongo import PyMongo
+
+# Create a flask app
+app = Flask(
+  __name__,
+  template_folder='templates',
+  static_folder='static'
+)
+## Mongo Configurations
+app.config["MONGO_DBNAME"] = "" # Your database name here
+app.config["MONGO_URI"] = "" # Your URI here (w/ username and password)
+mongo = PyMongo(app)
+
+#### ROUTES 
+# Index page
+@app.route('/')
+@app.route('/index')
+def index():
+  return render_template("index.html")
+
+
+## These run configurations are necessary for Flask to run here in Replit
+## The host must be 0.0.0.0 and the port must be 8080
+if __name__ == '__main__':
+  # Run the Flask app
+  app.run(
+	host='0.0.0.0',
+	debug=True,
+	port=8081
+  )
---
+++ model.py
@@ -0,0 +1,6 @@
+def rate_breakfast(breakfast):
+  if breakfast == "eggs":
+    return "That's a 5-star breakfast!"
+  elif breakfast == "biscuits and gravy":
+    return "That sounds like a southern treat!"
+  return "I hope you enjoyed it!"
